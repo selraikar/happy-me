493d26367be5cb92f725ba74ba07343568358688
User <User@192.168.0.202>
2023-02-22 22:03:35 +0530

    Happy me commit

---
+++ Hello/src/food/Journal_QI_1.java
@@ -0,0 +1,18 @@
+package food;
+import java.util.Scanner;
+public class Journal_QI_1 {
+	 
+	 
+		public static void main(String[]args){ 
+			 int a; 
+		Scanner in=new Scanner(System.in);
+		System.out.println("Enteranumber:");
+		a=in.nextInt(); 
+		if(a%2!=0&&a>=100){ 
+		System.out.println("Success");
+		} 
+		} 
+		} 
+
+
+
